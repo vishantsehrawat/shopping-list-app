b22d928bc7e5310ef840a5329620d2d211cca63b
vishant <vishant.96.sehrawat@gmail.com>
2022-10-12 13:48:15 +0530

    new components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeComponent,
+    ShoppingListComponent,
+    RecipeListComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core"; // import component from angular core
+
+@Component({  //component decorator
+    selector:'app-header',
+    templateUrl: './header.component.html',
+    styles: ['./header.component.css']
+})
+
+export class HeaderComponent{
+
+}
